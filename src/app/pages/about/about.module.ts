d0149e9eb94cbf29aabbe5b7bda5ddcf2de35fcd
yariv <yariv@aboveit.co.il>
2021-06-08 16:46:58 +0300

    about links

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import {AboutRoutingModule} from './about-routing.module';
 import {PanelModule} from 'primeng/panel';
-
+import {ButtonModule} from 'primeng/button';
 @NgModule({
   declarations: [
     AboutComponent
+
   ],
   imports: [
     CommonModule,
     PanelModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    ButtonModule,
   ]
 })
 export class AboutModule { }
